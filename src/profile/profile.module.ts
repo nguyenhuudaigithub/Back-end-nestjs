7ecd0fa1b8bea0f4e9b0446308b14855e097423c
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-08-01 11:10:25 +0700

    fix bug

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Profile, ProfileSchema } from './schemas/profile.schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema }]),
+  ],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
